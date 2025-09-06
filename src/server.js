4677ac67734d33f10c6e45837ef23ba1703ba128
suhaas3 <suhaas3w@gmail.com>
2025-09-06 19:07:32 +0530

    error handling using the try, catch, throw

--- src/server.js
+++ src/server.js
@@ -8,21 +8,46 @@ const app = express();
 // Define a port
 const PORT = 3333;
 
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(404).send("unexpected server error");
+  }
+})
+
+app.use("/user", (req, res, next) => {
+  try {
+    throw new Error("errorrrrrrrrrr");
+    res.send("user data fetched");
+  } catch (err) {
+    next(err); //pass this error to Express error handler
+  }
+})
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(404).send("unexpected server error");
+  }
+})
+
+
+/*
 app.use('/admin', adminAuth)
-
+ 
 app.use("/user", userAuth)
-
+ 
 app.get("/admin/getUserData", (req, res) => {
   res.send("All user Data")
 })
-
+ 
 app.get("/admin/deleteUser", (req, res) => {
   res.send("Deleted user successfully!");
 })
-
+ 
 app.get("/user/login", (req, res) => {
   res.send("user Data Fetched succesfully!");
 })
+*/
 
 /*
 app.use('/user',
@@ -31,25 +56,25 @@ app.use('/user',
     // res.send("1st route handler!");
     next();
   },
-
+ 
   (req, res, next) => {
     console.log('handle route -2');
     // res.send("2nd route handler!");
     next();
   }],
-
+ 
   (req, res, next) => {
     console.log("handle route -3");
     // res.send("3rd route handler!");
     next();
   },
-
+ 
   (req, res, next) => {
     console.log("handle route -4");
     // res.send("4th route handler!");
     next();
   },
-
+ 
    (req, res, next) => {
     console.log("handle route -5");
     res.send("5th route handler!");
@@ -59,22 +84,22 @@ app.use('/user',
 
 
 /*
-
+ 
 //if don't write the 'b' it will work
 app.get('/ab?cd',(req,res) => {
   console.log(res.query)
   res.send({firstname:"sai suhaas",lastname:"jatangi"});
 })
-
+ 
 //first letter is 'a' and then last letter is 'b' remaining any thing we write it will work
 app.get('/a*b',(req,res) => {
   res.send({first:"sai",last:"suhaas"})
 })
-
+ 
 app.get('/a(bd)+c',(req,res) => {
   res.send({first:"jatangi",last:"sai suhaas"})
 })
-
+ 
 app.get('/user/:userId/:userName/:password',(req,res) => {
   console.log(req.params)
   res.send({firstname:'hardik',lastname:'pandya'})
@@ -83,23 +108,23 @@ app.get('/user/:userId/:userName/:password',(req,res) => {
 app.get('/test',(req,res) => {
   res.send('tested data here there!');
 })
-
-
+ 
+ 
 app.post('/hello', async (req,res) => {
   console.log(req.body,'req body');
   //saving data to DB
   res.send("data posted succesfully!");
 })
-
+ 
 app.delete('/remove',(req,res) => {
   res.send("deleted data succesfully!");
 })
-
+ 
 //this will match all the HTTP method API calls to /test
 app.use('/test',(req,res) => {
   res.send("data get from the Test!");
 })
-
+ 
 */
 
 // Start the server
