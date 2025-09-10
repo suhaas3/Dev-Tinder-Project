e9e743b21dd8365f2d80921b9b8ef01e4c5917a3
suhaas3 <suhaas3w@gmail.com>
2025-09-10 08:15:18 +0530

    Adding validators on Schema level

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const { default: isURL } = require('validator/lib/isURL');
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -15,11 +17,21 @@ const userSchema = new mongoose.Schema({
     lowercase: true, // Always convert `test` to lowercase
     required: true,
     unique: true,
-    trim: true
+    trim: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Invalid Email:'+value);
+      }
+    }
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    validate(value) {
+      if (!validator.isStrongPassword(value)) {
+        throw new Error("Invalid password");
+      }
+    }
   },
   age: {
     type: Number,
@@ -30,6 +42,15 @@ const userSchema = new mongoose.Schema({
     validate(value) {
       if (!["male", "female", "other"].includes(value)) {
         throw new Error("gender error :" + value);
+      }
+    }
+  },
+  photoUrl: {
+    type: String,
+    default: "https://www.vhv.rs/dpng/d/256-2569650_men-profile-icon-png-image-free-download-searchpng.png",
+    validate(value) {
+      if (!validator.isURL(value)) {
+        throw new Error("Invalid photo URL");
       }
     }
   },
