9060441064035b4b504e042f2fd94d5fe3a9b69a
suhaas3 <suhaas3w@gmail.com>
2025-09-23 19:32:02 +0530

    simple change in userAuth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      throw new Error("Invalid Token!!!");
+      return res.status(401).send("Please Login!");
     }
     //verify a token
     const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
