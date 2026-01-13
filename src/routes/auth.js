aec5b38cfd1d257931403e5fb1d676f3cab6105d
suhaas3 <suhaas3w@gmail.com>
2026-01-13 13:14:08 +0530

    chnade some db connectiosn

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -21,29 +21,19 @@ authRouter.post("/signup", async (req, res) => {
     const user = new User({
       firstName, lastName, emailId, password: passwordHash
     });
-
+  
     const savedUser = await user.save();
     const token = await savedUser.getJwt();
 
-    // res.cookie("token", token, {
-    //   expires: new Date(Date.now() + 8 * 3600000)
-    // })
-
     res.cookie("token", token, {
-      httpOnly: true,
-      sameSite: "lax",      // ⭐ REQUIRED
       expires: new Date(Date.now() + 8 * 3600000)
-    });
+    })
 
-    res.json({
-      message: "user signup successfully!",
+    res.json({message :"user signup successfully!",
       data: savedUser
     });
   } catch (err) {
-    // res.status(400).send("Bad request:" + err.message);
-    res.status(400).json({
-      message: err.message
-    });
+    res.status(400).send("Bad request:" + err.message);
   }
 })
 
@@ -84,7 +74,7 @@ authRouter.post('/login', async (req, res) => {
 
 authRouter.post('/logout', async (req, res) => {
   res.cookie("token", null, {
-    expires: new Date(Date.now())
+    expires:  new Date(Date.now())
   }).send("logout successfully...");
 })
 
